b3218e514c80e450195a44650834590f71c784a9
Wang Chi <hiwangchi@gmail.com>
2018-10-29 14:50:57 +0800

    first commit, v0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # copy-webpack-output-plugin
 A webpack plugin to copy your output files to somewhere
+
+## Install
+
+```bash
+npm i copy-webpack-output-plugin --save-dev
+```
+
+## Usage
+
+```js
+const CopyWebpackOutputPlugin = require('./copy-webpack-output-plugin');
+
+plugins: [
+  new CopyWebpackOutputPlugin([{ src: '*.js', dest: 'foo' }], options)
+]
+```
+The arguments `src` and `dest` are glob patterns like  [node-glob](https://github.com/isaacs/node-glob#glob-primer).
+
+See the [options document](https://github.com/isaacs/node-glob#options) for usage detail and available methods.
+
+## Example
+
+In this example, we will copy vendor files to cdn folder, copy js and css files to static folder.
+
+```js
+const CopyWebpackOutputPlugin = require('./copy-webpack-output-plugin');
+
+module.exports = {
+  //...
+  plugins: [
+    new CopyWebpackOutputPlugin([
+      {
+        src: './dist/vendor.*',
+        dest: './cdn'
+      },
+      {
+        src: ['./dist/main.js', './dist/main.css'],
+        dest: './static',
+        options: {
+          // will overwrite outside options nodir:true
+          nodir: false
+        }
+      }
+    ], {
+      nodir: true
+    })
+  ]
+}
+```
+
+## License
+
+[MIT LICENSE](./LICENSE)
---
+++ index.js
@@ -0,0 +1,55 @@
+const copy = require('copy');
+const chalk = require('chalk');
+
+const isObj = (obj) => {
+  if (typeof(obj) === 'object' && Object.prototype.toString.call(obj).toLowerCase() === '[object object]') {
+    return true;
+  }
+  return false;
+}
+
+const copyPromise = (src, dest, options) => {
+  return new Promise((resolve, reject) => {
+    copy(src, dest, options || {}, (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    });
+  });
+};
+
+class CopyWebpackOutputPlugin {
+  constructor(patterns, options) {
+    this.pluginName = 'copy-webpack-output-plugin';
+
+    if (typeof options !== 'undefined' && !isObj(options)) {
+      console.log(chalk.red(this.pluginName) + ' options must be a object');
+      return;
+    }
+
+    patterns.map(item => {
+      if (item.hasOwnProperty('options') && !isObj(item.options)) {
+        console.log(chalk.red(this.pluginName) + ' options in patterns must be a object');
+        return;
+      }
+      item.options = Object.assign({}, options, item.options || {});
+    });
+
+    this.patterns = patterns;
+  }
+
+  apply(compiler) {
+    compiler.hooks.done.tap('CopyWebpackOutputPlugin', () => {
+      this.patterns.forEach(item => {
+        copyPromise(item.src, item.dest, item.options).then(files => {
+          files.forEach(file => {
+            console.log('copy ' +  chalk.bold.green(item.src) + ' to ' + chalk.bold.green(item.dest) + ' success!');
+          });
+        }).catch(err => {
+          throw err;
+        });
+      });
+    });
+  }
+}
+
+module.exports = CopyWebpackOutputPlugin;
